399986ac645fa7875c396b9eeb2fc5d42e99eea7
rkcrayton <raheemcrayton8@gmail.com>
2026-02-24 00:09:56 -0600

    Fixed the network  error on scan

--- apps/mobile/services/api.ts
+++ apps/mobile/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 import { Platform } from "react-native";
 
-const getBaseURL = () => {
+export const getBaseURL = () => {
   if (process.env.EXPO_PUBLIC_API_BASE_URL) {
     return process.env.EXPO_PUBLIC_API_BASE_URL;
   }
